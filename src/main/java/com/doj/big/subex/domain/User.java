e4a661ef1384c01885140e31b33541a97e7930c0
dineshonjava <admin@dineshonjava.com>
2015-04-07 21:54:14 +0530

    shared

--- src/main/java/com/doj/big/subex/domain/User.java
+++ src/main/java/com/doj/big/subex/domain/User.java
@@ -1,9 +1,35 @@
 package com.doj.big.subex.domain;
 
+import java.io.Serializable;
+
 /**
  * @author Dinesh.Rajput
  *
  */
-public class User {
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5427840854992462665L;
 	//To do
+	
+    private String username;
+    private String password;
+    
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/doj/big/subex/domain/support/UserBuilder.java
+++ src/main/java/com/doj/big/subex/domain/support/UserBuilder.java
@@ -1,9 +1,27 @@
 package com.doj.big.subex.domain.support;
 
+import com.doj.big.subex.domain.User;
+
 /**
  * @author Dinesh.Rajput
  *
  */
-public class UserBuilder {
+public class UserBuilder  extends EntityBuilder<User>{
+
+	@Override
+    void initProduct() {
+        this.product = new User();
+    }
+
+    public UserBuilder credentials(String username, String password) {
+        this.product.setUsername(username);
+        this.product.setPassword(password);
+        return this;
+    }
+
+	@Override
+	User assembleProduct() {
+		return null;
+	}
 
 }
